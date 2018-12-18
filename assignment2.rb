942d5779432871c51a2b8ca9d926ac442724cfc9
ghrahul <ghrahul2016@gmail.com>
2018-12-18 12:53:48 +0000

    assignment 2 added

---
+++ assignment2.rb
@@ -0,0 +1,8 @@
+puts "Enter the first number"
+fnum = gets.chomp
+puts "Enter the second number"
+snum = gets.chomp
+puts "Addition of the numbers are #{fnum.to_i + snum.to_i}"
+puts "Multiplication of the numbers are #{fnum.to_i * snum.to_i}"
+puts "Division of the numbers are #{fnum.to_i / snum.to_i}"
+puts "Substraction of the numbers are #{fnum.to_i - snum.to_i}"
