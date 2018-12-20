9f84083c1dc2eb738f7a339f9627b73d38a80910
ghrahul <ghrahul2016@gmail.com>
2018-12-20 13:39:29 +0000

    Assignment 3 added related to hash do loop

---
+++ assignment3.rb
@@ -0,0 +1,28 @@
+# This is hash definition
+$dial_book = {"Burdwan" => "91" , "Kolkata" => "92" , "Neemrana" => "45" , "Delhi" => "46" , "Bangalore" => "47"}
+
+#To show a particular city code
+def lookup(city)
+  puts $dial_book[city]
+end
+# To show all the list of cities
+def all_city()
+  $dial_book.each {|k,v| puts k}
+end
+
+#Taking input
+loop do
+  puts "Do you want to check area code?(Y/N)"
+  answer = gets.chomp
+  if answer != "Y"
+    break
+  end
+  
+  puts "Available cities are: "
+  all_city()
+  
+  puts "What city are you looking for?"
+  city = gets.chomp
+  lookup(city)
+  
+end
